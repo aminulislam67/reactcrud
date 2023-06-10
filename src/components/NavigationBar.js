4dd83e38e737010e52ec946d458ded4aa8108224
aminulislam67 <98544001+aminulislam67@users.noreply.github.com>
2023-06-10 10:31:57 +0600

    1

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,28 +1,26 @@
-import React from 'react'
-
-
-import { Navbar, Nav,Container } from 'react-bootstrap';
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">My School App</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">My School App</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="./">Home</Nav.Link>
-            <Nav.Link href="./">Registration</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/">Registration</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="./addstudent">Add Student</Nav.Link>
+            <Nav.Link as={Link} to="/addstudent">Add Student</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 };
-
 
 export default NavigationBar;
